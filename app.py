ac265b16b2e282baa19cf832f084c1b2b7efaa10
daliss228 <dayanalissrodriguez@gmail.com>
2026-06-17 21:33:34 -0500

    creacion endpoint prediccion perfil ahorrador 2 casos

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from services.prediction_service import router
 
 app = FastAPI()
+
+app.include_router(router)
+
+# Health Check
 
 @app.get("/")
 def root():
     return {
-        "mensaje":"API SaveIQ funcionando"
+        "status": "ok",
+        "message": "SaveIQ API funcionando"
     }
